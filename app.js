35250e09f6161a48c87b29f10219d1e491064395
snow treetree <18817834076@163.com>
2020-05-14 11:24:31 +0800

    feat: jsonp 测试

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/jsonp',function(req,res,next){  
+    res.jsonp({status:'jsonp'});  
+ });  
+   
+ app.get('/json',function(req,res,next){   
+     res.jsonp({status:'json'});  
+ });  
+
+app.use(express.static('public'));
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
